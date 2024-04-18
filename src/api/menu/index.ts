040eb7a5136b28b83047281d4881795ad300b4a5
QianKun <qiankun521123@gmail.com>
2024-04-18 12:55:32 +0000

    update:菜单

--- src/api/menu/index.ts
+++ src/api/menu/index.ts
@@ -30,7 +30,7 @@ export function listMenus(queryParams: MenuQuery): AxiosPromise<MenuVO[]> {
  */
 export function getMenuOptions(): AxiosPromise<OptionType[]> {
   return request({
-    url: "/api/v1/menus/options",
+    url: "/menus/options",
     method: "get",
   });
 }
@@ -54,7 +54,7 @@ export function getMenuForm(id: number): AxiosPromise<MenuForm> {
  */
 export function addMenu(data: MenuForm) {
   return request({
-    url: "/api/v1/menus",
+    url: "/menus",
     method: "post",
     data: data,
   });
@@ -68,7 +68,7 @@ export function addMenu(data: MenuForm) {
  */
 export function updateMenu(id: string, data: MenuForm) {
   return request({
-    url: "/api/v1/menus/" + id,
+    url: "/menus/" + id,
     method: "put",
     data: data,
   });
@@ -81,7 +81,7 @@ export function updateMenu(id: string, data: MenuForm) {
  */
 export function deleteMenu(id: number) {
   return request({
-    url: "/api/v1/menus/" + id,
+    url: "/menus/" + id,
     method: "delete",
   });
 }
