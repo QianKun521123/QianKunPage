b189202d3f72af5d4adc24d9ee62056f0ce5d126
乾坤 <2278417331@qq.com>
2024-05-18 11:37:24 +0800

    add:账单明细

---
+++ src/api/money/bill/detail/index.ts
@@ -0,0 +1,66 @@
+import request from "@/utils/request";
+import { AxiosPromise } from "axios";
+import { BillGategoryPageVO,BillGategoryQuery,BillCategoryForm } from "./types";
+
+/**
+ * 获取角色分页数据
+ * @param queryParams
+ */
+export function getBillCategoryPage(queryParams: BillGategoryQuery): AxiosPromise<PageResult<BillGategoryPageVO[]>> {
+  return request({
+    url: "/bill/category/page",
+    method: "get",
+    params: queryParams,
+  });
+}
+/**
+ * 获取角色详情
+ *
+ * @param id
+ */
+export function getBillCategoryForm(id: number): AxiosPromise<BillCategoryForm> {
+  return request({
+    url: "/bill/category/" + id ,
+    method: "get",
+  });
+}
+/**
+ * 添加账单名称
+ *
+ * @param data
+ */
+export function addBillCategory(data: BillCategoryForm){
+  return request({
+    url: "/bill/category",
+    method: "post",
+    data: data,
+  });
+}
+
+
+/**
+ * 更新账单名称
+ *
+ * @param id
+ * @param data
+ */
+export function updateBillCategory( data: BillCategoryForm) {
+  return request({
+    url: "/bill/category",
+    method: "put",
+    data: data,
+  });
+}
+
+
+/**
+ * 批量删除账单名称，多个以英文逗号(,)分割
+ *
+ * @param ids
+ */
+export function deleteBillCategory(ids: string) {
+  return request({
+    url: "/bill/category/" + ids,
+    method: "delete",
+  });
+}
---
+++ src/views/money/bill/detail/index.vue
@@ -0,0 +1,268 @@
+<script setup lang="ts">
+import { getBillCategoryPage,getBillCategoryForm, addBillCategory, updateBillCategory, deleteBillCategory } from "@/api/money/bill/category";
+
+import { BillCategoryPageVO, BillCategoryQuery, BillCategoryForm } from "@/api/money/bill/name/types";
+
+
+const queryFormRef = ref(ElForm);
+const roleFormRef = ref(ElForm);
+
+const loading = ref(false);
+const ids = ref<number[]>([]);
+const total = ref(0);
+const queryParams = reactive<BillCategoryQuery>({
+  pageNum: 1,
+  pageSize: 10,
+});
+
+const roleList = ref<BillCategoryPageVO[]>();
+
+const dialog = reactive({
+  title: "",
+  visible: false,
+});
+
+const formData = reactive<BillCategoryForm>({
+  remark: "",
+  name: "",
+  status: 0,
+  deleted: 0,
+});
+
+const rules = reactive({
+  name: [{ required: true, message: "请输入账单名称", trigger: "blur" }],
+  remark: [{ required: true, message: "请输入账单备注", trigger: "blur" }],
+});
+
+
+/** 查询 */
+function handleQuery() {
+  loading.value = true;
+  getBillCategoryPage(queryParams)
+    .then(({ data }) => {
+      roleList.value = data.list;
+      total.value = data.total;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+}
+/** 重置查询 */
+function resetQuery() {
+  queryFormRef.value.resetFields();
+  queryParams.pageNum = 1;
+  handleQuery();
+}
+
+/** 行checkbox 选中事件 */
+function handleSelectionChange(selection: any) {
+  ids.value = selection.map((item: any) => item.id);
+}
+
+/** 打开分类表单弹窗 */
+function openDialog(roleId?: number) {
+  dialog.visible = true;
+  if (roleId) {
+    dialog.title = "修改分类";
+    getBillCategoryForm(roleId).then(({ data }) => {
+      Object.assign(formData, data);
+    });
+  } else {
+    dialog.title = "新增分类";
+  }
+}
+
+/** 分类保存提交 */
+function handleSubmit() {
+  roleFormRef.value.validate((valid: any) => {
+    if (valid) {
+      loading.value = true;
+      const roleId = formData.id;
+      if (roleId) {
+        updateBillCategory( formData)
+          .then(() => {
+            ElMessage.success("修改成功");
+            closeDialog();
+            resetQuery();
+          })
+          .finally(() => (loading.value = false));
+      } else {
+        addBillCategory(formData)
+          .then(() => {
+            ElMessage.success("新增成功");
+            closeDialog();
+            resetQuery();
+          })
+          .finally(() => (loading.value = false));
+      }
+    }
+  });
+}
+
+/** 关闭表单弹窗 */
+function closeDialog() {
+  dialog.visible = false;
+  resetForm();
+}
+
+/** 重置表单 */
+function resetForm() {
+  roleFormRef.value.resetFields();
+  roleFormRef.value.clearValidate();
+
+  formData.id = undefined;
+  formData.status = 0;
+}
+
+/** 删除角色 */
+function handleDelete(roleId?: number) {
+  const roleIds = [roleId || ids.value].join(",");
+  if (!roleIds) {
+    ElMessage.warning("请勾选删除项");
+    return;
+  }
+
+  ElMessageBox.confirm("确认删除已选中的数据项?", "警告", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  }).then(() => {
+    loading.value = true;
+    deleteBillCategory(roleIds)
+      .then(() => {
+        ElMessage.success("删除成功");
+        resetQuery();
+      })
+      .finally(() => (loading.value = false));
+  });
+}
+
+onMounted(() => {
+  handleQuery();
+});
+</script>
+
+<template>
+  <div class="app-container">
+    <div class="search-container">
+      <el-form ref="queryFormRef" :model="queryParams" :inline="true">
+        <el-form-item prop="keywords" label="关键字">
+          <el-input
+            v-model="queryParams.keywords"
+            placeholder="分类名称"
+            clearable
+            @keyup.enter="handleQuery"
+          />
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="handleQuery"
+            ><i-ep-search />搜索</el-button
+          >
+          <el-button @click="resetQuery"><i-ep-refresh />重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <el-card shadow="never" class="table-container">
+      <template #header>
+        <el-button type="success" @click="openDialog()"
+          ><i-ep-plus />新增</el-button
+        >
+        <el-button
+          type="danger"
+          :disabled="ids.length === 0"
+          @click="handleDelete()"
+          ><i-ep-delete />删除</el-button
+        >
+      </template>
+
+      <el-table
+        ref="dataTableRef"
+        v-loading="loading"
+        :data="roleList"
+        highlight-current-row
+        border
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55" align="center" />
+        <el-table-column label="分类名称" prop="name" min-width="100" />
+        <el-table-column label="备注" prop="remark" width="150" />
+
+        <el-table-column label="状态" align="center" width="100">
+          <template #default="scope">
+            <el-tag v-if="scope.row.status === 0" type="success">启用</el-tag>
+            <el-tag v-else type="info">停用</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="创建时间" prop="createTime" width="200" />
+        <el-table-column label="修改时间" prop="updateTime" width="200" />
+
+        <el-table-column fixed="right" label="操作" width="220">
+          <template #default="scope">
+            <el-button
+              type="primary"
+              size="small"
+              link
+              @click="openDialog(scope.row.id)"
+            >
+              <i-ep-edit />编辑
+            </el-button>
+            <el-button
+              type="primary"
+              size="small"
+              link
+              @click="handleDelete(scope.row.id)"
+            >
+              <i-ep-delete />删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <pagination
+        v-if="total > 0"
+        v-model:total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="handleQuery"
+      />
+    </el-card>
+
+    <!-- 角色表单弹窗 -->
+    <el-dialog
+      v-model="dialog.visible"
+      :title="dialog.title"
+      width="500px"
+      @close="closeDialog"
+    >
+      <el-form
+        ref="roleFormRef"
+        :model="formData"
+        :rules="rules"
+        label-width="100px"
+      >
+        <el-form-item label="分类名称" prop="name">
+          <el-input v-model="formData.name" placeholder="请输入分类名称" />
+        </el-form-item>
+        <el-form-item label="备注"  prop="remark" >
+          <el-input v-model="formData.remark" placeholder="请输入备注" />
+        </el-form-item>
+        <el-form-item label="状态" prop="status">
+          <el-radio-group v-model="formData.status">
+            <el-radio :label="0">正常</el-radio>
+            <el-radio :label="1">停用</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="handleSubmit">确 定</el-button>
+          <el-button @click="closeDialog">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
+
+  </div>
+</template>
